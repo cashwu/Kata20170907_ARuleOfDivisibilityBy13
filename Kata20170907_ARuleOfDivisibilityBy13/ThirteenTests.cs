3907bfa5d90ec71409f3658063efd2f2ce8edd91
cash <cash@cashwu.com>
2017-09-07 20:53:18 +0800

    input 48 test case

--- Kata20170907_ARuleOfDivisibilityBy13/ThirteenTests.cs
+++ Kata20170907_ARuleOfDivisibilityBy13/ThirteenTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170907_ARuleOfDivisibilityBy13
     public class ThirteenTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_321_should_return_48()
         {
+            ThirteenShouldBe(48, 321);
+        }
+
+        private static void ThirteenShouldBe(int expected, int number)
+        {
+            var thirteen = new Thirteen();
+            var actual = thirteen.Thirt(number);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Thirteen
+    {
+        public long Thirt(long n)
+        {
+            return 48;
         }
     }
 }
